0537fece1b7f5d47b30991686fceaf1dab75feba
Sadab76 <sadabuzzaman75@gmail.com>
2023-04-12 14:59:18 +0600

    modifying about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,37 +19,40 @@ const About = () => {
 
         {/* 2 */}
 
-        <div className="my-10 lg:my-20   lg:flex lg:justify-around">
+        <div className="my-10  lg:my-20 lg:flex lg:justify-around lg:space-x-24 ">
           <div>
             <img
               src={Ab}
               alt=""
-              className=" w-[200px] h-[200px] mx-auto lg:w-[380px] lg:h-[370px] rounded-[30px]"
+              className=" w-[200px] h-[200px] mx-auto lg:mx-5 lg:w-[1100px] lg:h-[370px] rounded-[30px]"
             />
           </div>
 
-          <div className="flex flex-col justify-center gap-10 my-10 lg:my-0">
-            <div className="mx-auto gap-3 md:mx-auto lg:mx-0 flex md:flex md:gap-8">
-              <div className="border-2 border-gray-400 w-36 h-48 md:w-80 md:h-40 rounded-[30px] flex flex-col justify-center items-center text-center ">
+          <div className="flex flex-col justify-center  items-center lg:items-start   gap-10   my-10 lg:my-0">
+            <div className="space-y-8   md:space-y-0  md:flex md:gap-8">
+
+              <div className="border-2 border-gray-400 w-80 h-40 rounded-[30px] flex flex-col justify-center items-center text-center ">
                 <SlBadge size={20} />
 
                 <p className="font-bold text-lg">Experience</p>
                 <p className="text-gray-700">2+ years</p>
                 <p className="text-gray-700">Frontend Development</p>
               </div>
-              <div className="border-2 border-gray-400 pt-5 md:pt-0 w-36 h-48 md:w-80 md:h-40 rounded-[30px] flex flex-col justify-center items-center text-center ">
+              <div className="border-2 border-gray-400 w-80 h-40 rounded-[30px] flex flex-col justify-center items-center text-center ">
                 <BsFillPeopleFill size={20} />
 
                 <p className="font-bold text-lg">Education</p>
                 <p className="text-gray-700">B.Sc. Bachelors Degree</p>
                 <p className="text-gray-700">M.Sc. Masters Degree</p>
               </div>
+              
             </div>
-            <div className="text-center lg:text-start">
-               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore harum voluptas, <br /> cumque voluptatem velit distinctio 
-               perferendis tenetur veniam mollitia nesciunt, cum quisquam aliquid <br />  dignissimos fugit dolorum magni. Dolore, tempore eaque?
+            <div className="text-justify px-10 lg:px-0">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore omnis sapiente hic odio quod voluptates alias nobis 
+            excepturi perspiciatis natus voluptatem exercitationem voluptas quibusdam deserunt impedit libero rerum adipisci  quos nisi velit sunt! Quibusdam magni  laborum perspiciatis, eaque quos nobis?
             </div>
           </div>
+         
         </div>
       </div>
     </>
